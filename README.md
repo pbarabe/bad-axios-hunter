a0d5c1ed03dae65b53d00bdbf32ae5d5c892d7a0
Patrick Barabe <pbarabe@arizona.edu>
2026-04-20 13:01:15 -0700

    Update README and streamline script

--- README.md
+++ README.md
@@ -1,8 +1,8 @@
-# Shai-Hulud Hunter #
+# Bad-Axios Hunter #
 
 ## About ##
 
-Searches for projects with dependencies that may have been compromised by [Shai-Hulud](https://www.cisa.gov/news-events/alerts/2025/09/23/widespread-supply-chain-compromise-impacting-npm-ecosystem).
+Searches for projects with dependencies that may have been compromised by malicious [axios / plain-crypto-js](https://content.govdelivery.com/accounts/USDHSCISA/bulletins/413c7a5).
 
 Uses [jq](https://jqlang.org/) to parse [affected-packages.json](./affected-packages.json) and then recursively `grep` each package name in lock files found in the named directory.
 
@@ -19,27 +19,29 @@ The `affected-packages.json` file was created from the list of known, affected p
 
 ```sh
 # Scan a named directory with package names extracted from affected-packages.json
-shaihulud-hunter.sh <directory> {affected-packages.json}
+bad-axios.sh <directory> {affected-packages.json}
 
 # Test scan on files in the ./test/ folder
-shailulud-hunter.sh -t
+bad-axios.sh -t
 ```
 
 
 ## Modification log ##
 
-23 Sep 2025
-- Initial creation  
+20 April 2025
+- Initial fork as 'bad-axios-hunter'
 
 24 Sep 2025
 - Refine logic to evaluate only package-lock.json, yarn.lock, and pnpm-lock.yaml files  
 - Replace affected-packages.json contents with more up-to-date list grabbed from https://socket.dev/
 
+23 Sep 2025
+- Initial creation (shai-hlud-hunter)
+
 
 ## Additional Resources ##
 
-- <https://www.cisa.gov/news-events/alerts/2025/09/23/widespread-supply-chain-compromise-impacting-npm-ecosystem>
-- <https://unit42.paloaltonetworks.com/npm-supply-chain-attack/>
-- <https://socket.dev/blog/ongoing-supply-chain-attack-targets-crowdstrike-npm-packages>
-- <https://www.reversinglabs.com/blog/shai-hulud-worm-npm>
+- <https://github.com/axios/axios/issues/10636>
+- <https://www.microsoft.com/en-us/security/blog/2026/04/01/mitigating-the-axios-npm-supply-chain-compromise/>
+- <https://socket.dev/blog/axios-npm-package-compromised>
 
--- bad-axios-hunter.sh
+++ bad-axios-hunter.sh
@@ -1,42 +1,33 @@
 #!/usr/bin/env bash
 #
 # Name:
-#   shaihulud-hunter.sh
+#   bad-axios-hunter.sh
 #
 # About:
-#   Uses jq to parse the JSON file and then recursively `grep` each package
-#   name in the parent directory.
-#
-#   The affected-packages.json file was created from the list of known, affected
-#   packages published by reversinglabs.com at
-#   https://www.reversinglabs.com/blog/shai-hulud-worm-npm
+#   Recursively `grep` each package lock files in a directory for
+#   plain-crypto-js and vulnerable versions of axios.
 #
 # Prerequites:
-#   bash, jq
+#   bash
 #
 # Usage:
-#   shaihulud-hunter.sh <directory> {affected-packages.json}
+#   shaihulud-hunter.sh <directory>
 #
 # Author:
 #   Patrick Barabe <pbarabe@arizona.edu>
 #
 # Modification log:
-#   23 Sep 2025 - Initial creation
+#   20 Apr 2026 - Forked and adapted as bad-axios-hunter
 #   24 Sep 2025 - Refine logic to evaluate only package-lock.json files
+#   23 Sep 2025 - Initial creation (shai-hulud-hunter.sh)
 
 # Check if the correct number of arguments is provided
 if [ "$#" -ne 1 ]; then
-    echo "Usage: $0 <directory_name> {affected-packages.json}"
-    echo "       Search <directory_name> using patterns from optional {affected-packages.json}"
+    echo "Usage: $0 <directory_name>"
+    echo "       Search <directory_name>"
     echo ""
     echo "Usage: $0 -t"
     echo "       Scan test files in ./test/ dir"
-    exit 1
-fi
-
-# Check if jq is installed
-if ! command -v jq &> /dev/null; then
-    echo "jq is required but not installed. Please install jq."
     exit 1
 fi
 
@@ -45,14 +36,10 @@ script_directory=$(dirname -- $(readlink -fn -- "$0"; echo x))
 
 # Assign arguments to variables
 search_directory="$1"
-packages_file="${2:-$script_directory/affected-packages.json}"
 
 if [ "$1" == "-t" ]; then
   search_directory=$(dirname -- $(readlink -fn -- "$0"; echo x))
 fi
-
-# Extract package names from the JSON file
-package_names=$(jq -r '.[].package' "$packages_file")
 
 # Create a temporary file to store the results
 temp_file=$(mktemp)
@@ -60,11 +47,9 @@ temp_file=$(mktemp)
 # Find all package-lock.json files and search for package names
 find "$search_directory" -name "package-lock.json" -o -name "yarn.lock" -o -name "pnpm-lock.yaml" | while read -r lock_file; do
     echo "Scanning $lock_file"
-    for package in $package_names; do
-        if grep -q "$package" "$lock_file"; then
-            echo -e "\e[31mFound package\e[0m '$package' in $lock_file" >> "$temp_file"
-        fi
-    done
+    if grep -E "axios@(1\.14\.1|0\.30\.4)|plain-crypto-js" "$lock_file"; then
+        echo -e "\e[31mFound package\e[0m plain-crypto-js in $lock_file" >> "$temp_file"
+    fi
 done
 
 # Display results
@@ -75,7 +60,7 @@ if [ -s "$temp_file" ]; then
     echo ""
 else
     echo ""
-    echo -e "\e[32mOK: \e[0mNo packages found in any package-lock.json, yarn.lock, or pnpm-lock.yaml files."
+    echo -e "\e[32mOK: \e[0mplain-crypto-js package not found in any package-lock.json, yarn.lock, or pnpm-lock.yaml files."
     echo ""
 fi
 
